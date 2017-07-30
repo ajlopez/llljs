42590eb2e0df6eacdbfc0d198b5d612328a24ac1
ajlopez <webmaster@ajlopez.com>
2017-07-30 15:56:14 -0300

    Lexer get string

--- test/lexers.js
+++ test/lexers.js
@@ -38,3 +38,27 @@ exports['get integer'] = function (test) {
 	test.equal(lexer.nextToken(), null);
 };
 
+exports['get integer with spaces'] = function (test) {
+	var lexer = lexers.lexer('  42\n\r\t ');
+	
+	var result = lexer.nextToken();
+	
+	test.ok(result);
+	test.equal(result.type, TokenType.Integer);
+	test.equal(result.value, '42');
+	
+	test.equal(lexer.nextToken(), null);
+};
+
+exports['get string'] = function (test) {
+	var lexer = lexers.lexer('"foo"');
+	
+	var result = lexer.nextToken();
+	
+	test.ok(result);
+	test.equal(result.type, TokenType.String);
+	test.equal(result.value, 'foo');
+	
+	test.equal(lexer.nextToken(), null);
+};
+
