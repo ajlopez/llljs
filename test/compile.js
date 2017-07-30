2301453b8c583e970e687a44ed03fc46981dda1b
ajlopez <webmaster@ajlopez.com>
2017-07-30 18:05:58 -0300

    Compile push with more than one byte

--- test/compile.js
+++ test/compile.js
@@ -7,3 +7,9 @@ exports['compile integer as push'] = function (test) {
 	test.equal(result, '602a');
 };
 
+exports['compile integer as push with two bytes'] = function (test) {
+	var result = lll.compile('256');
+	
+	test.equal(result, '610100');
+};
+
