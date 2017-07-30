f9c6da83975c54e07227d89ee95638a92ef1789b
ajlopez <webmaster@ajlopez.com>
2017-07-30 15:28:04 -0300

    Lexer get name

---
+++ test/lexers.js
@@ -0,0 +1,15 @@
+
+var lexers = require('../lib/lexers');
+var TokenType = lexers.TokenType;
+
+exports['get name'] = function (test) {
+	var lexer = lexers.lexer('foo');
+	
+	var result = lexer.nextToken();
+	
+	test.ok(result);
+	test.equal(result.type, TokenType.Name);
+	test.equal(result.value, 'foo');
+	
+	test.equal(lexer.nextToken(), null);
+};
