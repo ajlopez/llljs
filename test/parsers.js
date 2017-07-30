a58d56e248c80536a171061231cf27f54481cf3b
ajlopez <webmaster@ajlopez.com>
2017-07-30 17:01:22 -0300

    Parse integer

---
+++ test/parsers.js
@@ -0,0 +1,27 @@
+
+var parsers = require('../lib/parsers');
+
+exports['parse integer'] = function (test) {
+	var parser = parsers.parser('42');
+	
+	var expr = parser.parse();
+	
+	test.ok(expr);
+	test.equal(expr.toString(), '42');
+	test.equal(expr.compile(), '2a');
+	
+	test.equal(parser.parse(), null);
+};
+
+exports['parse one digit integer'] = function (test) {
+	var parser = parsers.parser('3');
+	
+	var expr = parser.parse();
+	
+	test.ok(expr);
+	test.equal(expr.toString(), '3');
+	test.equal(expr.compile(), '03');
+	
+	test.equal(parser.parse(), null);
+};
+
